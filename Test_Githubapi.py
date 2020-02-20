e470abe80ffa8e68b595af9dddb05f2d21f0f777
capWailing <thomasyantom@126.com>
2020-02-19 21:33:38 -0500

    Complete Githubapi.py

--- Test_Githubapi.py
+++ Test_Githubapi.py
@@ -6,11 +6,8 @@ class TestGithubApi(unittest.TestCase):
     def test_form_restful(self):
         self.assertEqual(form_restful("CapWailing"), "https://api.github.com/users/CapWailing")
 
-    def test_get_repos(self):
-        self.assertEqual(get_repos("https://api.github.com/users/CapWailing/repos"), 0)
-
     def test_get_commits(self):
-        self.assertEqual(get_commits("https://api.github.com/repos/CapWailing/SSW567-Triangle", 11))
+        self.assertEqual(get_commits("https://api.github.com/repos/CapWailing/SSW567-Triangle/commits"), 11)
 
 
 if __name__ == '__main__':
